e0e80d4b414a56cd57b23f9c9eb8088e66f08d33
Hugh O'Reilly <G00364715@GMIT.IE>
2019-11-14 11:39:04 +0000

    week5 labs

--- Week 5/server/a_simpleserver.py
+++ Week 5/server/a_simpleserver.py
@@ -1,9 +1,14 @@
 #!flask/bin/python
 from flask import Flask
 
+app = Flask(__name__,
+            static_url_path='',
+            static_folder='../')
+
+
 @app.route('/')
 def index():
     return "hello world"
 
-if __name_=='__main__':
+if __name__=='__main__':
     app.run(debug=True)
