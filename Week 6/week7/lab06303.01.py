1ef474c38719c0dd60d267c8aab4b7c206708c30
Hugh O'Reilly <G00364715@GMIT.IE>
2019-11-18 15:40:31 +0000

    test file updated

--- Week 6/week7/lab06303.01.py
+++ Week 6/week7/lab06303.01.py
@@ -3,24 +3,31 @@ import requests
 from github.Commit import Commit
 from github.ContentFile import ContentFile
 # remove the minus sign from the key
-g = Github("de5a5b9cefe7be45eeec0d60e32474e1f003b345-")
+g = Github("")
 
 
 #for repo in g.get_user("oreillyh").get_repos("dataRepresentation"):
-    #print(repo.name)
+ #   print(repo.name)
 
-#g = Github("g = Github("de5a5b9cefe7be45eeec0d60e32474e1f003b345-")
+#g = Github("g = Github("")
 repo = g.get_repo("oreillyh/dataRepresentation")
-print(repo.clone_url)
-repo.create_file("test.txt", "test", "test")
-{'content': ContentFile(path="oreillyh/dataRepresentation/test.txt"), 'commit': Commit(sha="de5a5b9cefe7be45eeec0d60e32474e1f003b345-")}
 
-# ContentFile(path="oreillyh/dataRepresentation/test.txt") 
-# Commit(sha="de5a5b9cefe7be45eeec0d60e32474e1f003b345-")
-
-# print(repo.clone_url)")
-# repo = g.get_repo("datarepresentationstudent/aPrivateOne")
 # print(repo.clone_url)
 # print (urlOfFile)
-# pip install PyGithub
-# g = Github("de5a5b9cefe7be45eeec0d60e32474e1f003b345-")
+
+fileInfo = repo.get_contents("test.txt")
+urlOfFile = fileInfo.download_url
+
+#print(urlOfFile)
+
+response = requests.get(urlOfFile)
+contentOfFile = response.text
+#print(contentOfFile)
+
+newContents = contentOfFile + "more stuff \n"
+
+#print(newContents)
+
+GitHubResponse = repo.update_file(fileInfo.path, "updated by prog", newContents,fileInfo.sha)
+
+print(GitHubResponse)
