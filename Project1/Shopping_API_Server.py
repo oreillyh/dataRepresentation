66f4577569d8911265a9178a32882d77bc50814a
Hugh O'Reilly <G00364715@GMIT.IE>
2019-12-01 20:19:43 +0000

    grocerysDAO added

--- Project1/Shopping_API_Server.py
+++ Project1/Shopping_API_Server.py
@@ -1,92 +1,94 @@
 #!flask/bin/python
-from flask import Flask, jsonify,  request, abort, make_response
+from flask import Flask, jsonify,  request, abort,  make_response
+from flask_cors import CORS
+from ShoppingDAO import ShoppingDAO
 
 app = Flask(__name__,
             static_url_path='', 
             static_folder='../')
+CORS (app)
 
 groceries = [
     {
         "id":"1",
-        "type":"Fruit",
+        "item":"Fruit",
         "name":"oranges",
-        "quantity":6
+        "quantity":6,
         
     },
     {
         "id":"2",
-        "type":"Tins",
+        "item":"Tins",
         "name":"beans",
-        "quantity":3
+        "quantity":3,
         
     },
     {
         "id":"3",
-        "type":"DryGroceries",
+        "item":"DryGroceries",
         "name":"pasta",
-        "quantity":1
+        "quantity":1,
         
     },
     {
         "id":"4",
-        "type":"Sauces",
+        "item":"Sauces",
         "name":"pepper",
-        "quantity":1
+        "quantity":1,
         
     },
     {
         "id":"5",
-        "type":"Meat",
+        "item":"Meat",
         "name":"steak",
-        "quantity":7
+        "quantity":6,
         
     },
     {
         "id":"6",
-        "type":"Vegetables",
+        "item":"Vegetables",
         "name":"onions",
-        "quantity":6
+        "quantity":6,
         
     },
     {
         "id":"7",
-        "type":"Meat",
+        "item":"Meat",
         "name":"chicken",
-        "quantity":7
+        "quantity":5,
     },
     {
         "id":"8",
-        "type":"Vegetables",
+        "item":"Vegetables",
         "name":"potatoes",
-        "quantity":10
+        "quantity":10,
         
     },
     {
         "id":"9",
-        "type":"Sauces",
+        "item":"Sauces",
         "name":"carbonara",
-        "quantity":6
-        
+        "quantity":3,
     },
     {
-        "id":"10",
-        "type":"Fruit",
+         "id":"10",
+        "item":"Fruit",
         "name":"apples",
-        "quantity":6
-    }
+        "quantity":6,
+    },
 ]
 
 @app.route('/groceries', methods=['GET'])
-def get_Groceries():
-    return jsonify( {'grocery':groceries})
-# curl -i http://localhost:5000/groceries/
+def get_AllGroceries():
+    return jsonify( {'groceries':groceries})
+# curl -i "http://localhost:5000/groceries/"
 
 @app.route('/groceries/<string:id>', methods =['GET'])
-def get_Groceries(id):
+def get_groceries(id):
     foundGroceries = list(filter(lambda t : t['id'] == id , groceries))
     if len(foundGroceries) == 0:
-        return jsonify( { 'grocerY' : '' }),204
-    return jsonify( { 'grocery' : foundGroceries[0] })
+        return jsonify( { 'groceries' : '' }),204
+    return jsonify( { 'groceries' : foundGroceries[0] })
 
 @app.route('/groceries', methods=['POST'])
 def create_groceries():
@@ -96,38 +98,38 @@ def create_groceries():
         abort(400)
     groceries={
         "id":  request.json['id'],
-        "type": request.json['type'],
+        "item": request.json['item'],
         "name":request.json['name'],
         "quantity":request.json['quantity']
     }
     groceries.append(groceries)
-    return jsonify( {'grocery':groceries }),201
+    return jsonify( {'groceries':groceries }),201
 
-#curl -i http://localhost:5000/groceries/test
+#curl -i "http://localhost:5000/groceries/test"
 
 @app.route('/groceries/<string:id>', methods =['PUT'])
-def update_car(id):
+def update_groceries(id):
     foundGroceries=list(filter(lambda t : t['id'] ==id, groceries))
     if len(foundGroceries) == 0:
         abort(404)
     if not request.json:
         abort(400)
-    if 'type' in request.json and type(request.json['type']) != str:
+    if 'item' in request.json and type(request.json['item']) != str:
         abort(400)
     if 'name' in request.json and type(request.json['name']) is not str:
         abort(400)
     if 'quantity' in request.json and type(request.json['quantity']) is not int:
         abort(400)
     
-    foundGroceries[0]['type']  = request.json.get('type', foundGroceries[0]['type'])
+    foundGroceries[0]['item']  = request.json.get('item', foundGroceries[0]['item'])
     foundGroceries[0]['name'] =request.json.get('name', foundGroceries[0]['name'])
     foundGroceries[0]['quantity'] =request.json.get('quantity', foundGroceries[0]['quantity'])
-    return jsonify( {'grocery':foundGroceries[0]})
+    return jsonify( {'groceries':foundGroceries[0]})
 
-#curl -i -H "Content-Type:application/json" -X PUT -d "{\"quantity\":\"10\"}" http://localhost:5000/groceries/1
+#curl -i -H "Content-item:application/json" -X PUT -d "{\"quantity\":\"10\"}" http://localhost:5000/groceries/1
 
 app.route('/groceries/<string:id>', methods =['DELETE'])
-def delete_car(id):
+def delete_groceries(id):
     foundGroceries = list(filter (lambda t : t['id'] == id, groceries))
     if len(foundGroceries) == 0:
         abort(404)
